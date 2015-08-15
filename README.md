85e9cd676dd8f5c7cf8c9fff8c07cf7461cf1187
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2015-08-15 17:11:31 -0400

    Roadmap

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 A Robot with human manners
 
+[![asciicast](https://asciinema.org/a/25082.png)](https://asciinema.org/a/25082)
+
+## TODO:
+
+    - Fuzzy string match for commands
+    - Commands receive message context.
+    - Listen all messages to trigger commands.
+    - Adapters for Slack, XMPP, Twitter.
+    - Message stream filters.
+    - Message stream middlewares.
+    - Natural Language Processing?.
+    - Stream messages
+
+## Future examples
+
+    - Auto linking for Jira Issues.
+    - Attach user mentions to issues as comments in Jira.
+    - Bots talking to bots.
+    - Voting and Polls
--- examples/example1.py
+++ examples/example1.py
@@ -6,19 +6,32 @@ Example robot
 """
 from datetime import datetime
 from pengbot.api import command, env
+# from pengbot.api import listen
 
 
-@command(alias=['hi', 'hello'])
-def say_hi():
+@command(alias=['hi'])
+def hello():
     return 'Hello'
 
 
-@command(alias='now')
+@command
+def bye():
+    return 'Good bye'
+
+
+@command(alias=['now', 'what time is it?'])
 def say_time():
     """Return the current time"""
     return 'The current date and time is {}'.format(datetime.now())
+
 
 @command
 def dance():
     """Dancing music!"""
     return 'Dancing!'
+
+
+# @listen
+# def jira_issue(message, context):
+#     if message.match('(?P<issue>[A-Z]{2,}-\d+)'):
+#         pass
